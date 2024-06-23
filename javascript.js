49f8bc63eb114fbfc695c7faed323447aa38dacd
xachlysss <160469724+xachlysss@users.noreply.github.com>
2024-06-23 19:11:24 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,3 +10,42 @@ document.addEventListener("keydown", function (event){
 document.addEventListener('contextmenu', 
     event => event.preventDefault()
 );
+
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+        event.preventDefault();
+    }
+    if(event.keyCode == 123){
+        event.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', 
+    event => event.preventDefault()
+);
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a [href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
