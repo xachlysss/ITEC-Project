2454d8877c479e46de28c63f2d37977aba3a6dd0
xachlysss <160469724+xachlysss@users.noreply.github.com>
2024-06-23 11:17:49 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+        event.preventDefault();
+    }
+    if(event.keyCode == 123){
+        event.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', 
+    event => event.preventDefault()
+);
